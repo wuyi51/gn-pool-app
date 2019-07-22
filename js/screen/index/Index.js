d0ad69cb3c25b55c1b9109a72d1562483b0865c1
1626239306@qq.com <dh014388>
2019-07-23 00:57:20 +0800

    首页

--- js/screen/index/Index.js
+++ js/screen/index/Index.js
@@ -54,6 +54,67 @@ class Index extends React.Component{
                         </View>
                     </View>
                 </ImageBackground>
+                <View style={ [GStyle.mgt15,GStyle.mgr15] }>
+                    <Text style={[GStyle.right,GStyle.textBlack]}>账户详情</Text>
+                </View>
+                <View style={ [GStyle.mgl0,GStyle.mgr10,GStyle.row,GStyle.mgt15] }>
+                    <View style={[styles.lineDouble,GStyle.flex12,GStyle.mgr10,GStyle.mgl10]}>
+                        <Image style={[styles.lineDoubleImg]} source={require('../../../res/image/sky_0.png')}></Image>
+                        {/*  通过height控制灰度范围，假设进度为60%，height值为（1-0.6）* 80    */}
+                        <View style={[styles.cover,{height: 32}]}>
+                            <Image style={[styles.coverImg]} source={require('../../../res/image/sky_0_gray.png')}></Image>
+                        </View>
+                        <View style={[styles.info,GStyle.pd10,GStyle.posColBetween]}>
+                            <Text style={[GStyle.textWhite]}>自有算力</Text>
+                            <Text style={[GStyle.textWhite,GStyle.textRight]}>0T/0T</Text>
+                        </View>
+                    </View>
+                    <View style={[styles.lineDouble,GStyle.flex12]}>
+                        <Image style={[styles.lineDoubleImg]} source={require('../../../res/image/sky_1.png')}></Image>
+                        <View style={[styles.cover,{height: 50}]}>
+                            <Image style={[styles.coverImg]} source={require('../../../res/image/sky_1_gray.png')}></Image>
+                        </View>
+                        <View style={[styles.info,GStyle.pd10,GStyle.posColBetween]}>
+                            <Text style={[GStyle.textWhite]}>自有算力</Text>
+                            <Text style={[GStyle.textWhite,GStyle.textRight]}>0T/0T</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={ [GStyle.mgl0,GStyle.mgr10,GStyle.row,GStyle.mgt15] }>
+                    <View style={[styles.lineDouble,GStyle.flex12,GStyle.mgr10,GStyle.mgl10]}>
+                        <Image style={[styles.lineDoubleImg]} source={require('../../../res/image/sky_2.png')}></Image>
+                        <View style={[styles.cover,{height: 17}]}>
+                            <Image style={[styles.coverImg]} source={require('../../../res/image/sky_2_gray.png')}></Image>
+                        </View>
+                        <View style={[styles.info,GStyle.pd10,GStyle.posColBetween]}>
+                            <Text style={[GStyle.textWhite]}>自有算力</Text>
+                            <Text style={[GStyle.textWhite,GStyle.textRight]}>0T/0T</Text>
+                        </View>
+                    </View>
+                    <View style={[styles.lineDouble,GStyle.flex12]}>
+                        <Image style={[styles.lineDoubleImg]} source={require('../../../res/image/sky_3.png')}></Image>
+                        <View style={[styles.cover,{height: 0}]}>
+                            <Image style={[styles.coverImg]} source={require('../../../res/image/sky_3_gray.png')}></Image>
+                        </View>
+                        <View style={[styles.info,GStyle.pd10,GStyle.posColBetween]}>
+                            <Text style={[GStyle.textWhite]}>自有算力</Text>
+                            <Text style={[GStyle.textWhite,GStyle.textRight]}>0T/0T</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={ [GStyle.mgl0,GStyle.mgr10,GStyle.row,GStyle.mgt15] }>
+                    <View style={[styles.lineSinger,GStyle.flex12,GStyle.mgl10]}>
+                        <Image style={[styles.lineSingerImg]} source={require('../../../res/image/sky_4.png')}></Image>
+                        {/*  通过height控制灰度范围，假设进度为60%，height值为（1-0.6）* 60    */}
+                        <View style={[styles.cover,{height: 10}]}>
+                            <Image style={[styles.coverImg]} source={require('../../../res/image/sky_4_gray.png')}></Image>
+                        </View>
+                        <View style={[styles.singer_info,GStyle.pd10,GStyle.posColBetween]}>
+                            <Text style={[GStyle.textWhite]}>自有算力</Text>
+                            <Text style={[GStyle.textWhite,GStyle.textRight]}>0T/0T</Text>
+                        </View>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -74,6 +135,54 @@ const styles = StyleSheet.create({
         width: 50,
         height: 20,
         borderRadius: 20
+    },
+    lineDouble: {
+        height: 80,
+        borderTopLeftRadius:8,
+        borderTopRightRadius: 8,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+        position: 'relative',
+        overflow: 'hidden'
+    },
+    info: {
+        height: 80,
+        width: '100%',
+        position: 'absolute'
+    },
+    singer_info: {
+        height: 60,
+        width: '100%',
+        position: 'absolute'
+    },
+    lineDoubleImg: {
+        height: 80,
+        width: '100%',
+        position: 'absolute'
+    },
+    cover: {
+        width: '100%',
+        position: 'absolute',
+        overflow: 'hidden',
+        backgroundColor: 'transparent'
+    },
+    coverImg: {
+        height: 80,
+        width: '100%',
+    },
+    lineSinger: {
+        height: 60,
+        borderTopLeftRadius:8,
+        borderTopRightRadius: 8,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+        position: 'relative',
+        overflow: 'hidden'
+    },
+    lineSingerImg: {
+        height: 60,
+        width: '100%',
+        position: 'absolute'
     }
 });
 
