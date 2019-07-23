ecf2e243d875b8b7db22b4536d65e944167fb050
1626239306@qq.com <dh014388>
2019-07-23 23:50:33 +0800

    chart

--- js/screen/index/AccountDetail.js
+++ js/screen/index/AccountDetail.js
@@ -10,9 +10,11 @@ import {
     Platform,
     BackHandler,
     ToastAndroid,
-    StyleSheet, ImageBackground
+    StyleSheet,
+    ImageBackground
 } from "react-native";
 import _ from "lodash";
+import { BarChart, Grid, YAxis, XAxis } from 'react-native-svg-charts'
 
 
 class AccountDetail extends React.Component{
@@ -34,6 +36,9 @@ class AccountDetail extends React.Component{
 
 
     render(){
+        const fill = 'rgb(234, 97, 102)'
+        const data = [540, 310, 440, 295, 314, 824, 611, 585, 423, 330, 635, 953, 513, 524, 350, 420, 480,350, 310, 640, 795, 354, 324, 411, 685, 523, 485,424, 750, 420, 680,]
+        const contentInset = { top: 30, bottom: 30 }
         return (
             <View style={ [GStyle.container] }>
                 <ImageBackground source={require('../../../res/image/sky.png')} style={[{width: '100%', height: 120},GStyle.pd20]}>
@@ -50,7 +55,41 @@ class AccountDetail extends React.Component{
                     </View>
                 </ImageBackground>
                 <ScrollView>
+                    <View style={[GStyle.row,GStyle.mgl20,GStyle.mgr20,{ height: 400 }]}>
+                        <YAxis
+                            data={data}
+                            contentInset={{top: 30, bottom: 30}}
+                            svg={{
+                                fill: 'black',
+                                fontSize: 10,
+                            }}
+                            style={{
 
+                            }}
+                            numberOfTicks={10}
+                            formatLabel={(value) => `${value}  `}
+                        />
+                        <View style={{flex: 1, marginLeft: 16}}>
+                            <BarChart style={{ height: 380}} data={data} svg={{ fill }} contentInset={contentInset}>
+                                <Grid />
+                            </BarChart>
+                            <XAxis
+                                style={{ marginHorizontal: -10 ,marginTop: 5}}
+                                data={data}
+                                formatLabel={(value, index) => {
+                                    if(index % 2 === 0){
+                                        return index + 1
+                                    }else {
+                                        return ''
+                                    }
+
+                                }}
+                                contentInset={{ left: 10, right: 10 }}
+                                svg={{ fontSize: 10, fill: 'black' }}
+                            />
+                        </View>
+
+                    </View>
                 </ScrollView>
             </View>
         );
